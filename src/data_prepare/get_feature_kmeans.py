b4b3582540c86b2f103d2daffb2eae7ba69f46bf
blackcat <81723334@qq.com>
2024-05-31 22:44:30 +0800

    feat:kmeans保存特征

--- src/data_prepare/get_feature_kmeans.py
+++
@@ -1,6 +0,0 @@
-from model.ResNet2d import generate_model
-
-
-
-if __name__ == "__main__":
-    ...
---
+++ src/get_feature_kmeans.py
@@ -0,0 +1,39 @@
+from model.ResNet2d import generate_model
+from dataset.PathologyDataset import PathologyDataset
+import torch
+from torch.utils.data import DataLoader
+from sklearn.cluster import KMeans
+
+
+def get_feature_kmeans(
+    data_path="../data/pathology_img_data",
+    model_path="../model/resnet50/resnet50_epoch_best_210.pth",
+    save_path="../model/kmeans.pth",
+):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = generate_model(50).to(device)
+    # 载入参数
+    model.load_state_dict(torch.load(model_path))
+    dataset = PathologyDataset(data_path)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=False, num_workers=16)
+    model.eval()
+    feature_list = []
+    for data, target in dataloader:
+        data, target = data.to(device), target.to(device)
+        features = model(data, mode="two")
+        for feature in features:
+            feature_list.append(feature.cpu().detach().numpy())
+    # 聚类
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(feature_list)
+    # 保存中心点
+    torch.save(kmeans.cluster_centers_, save_path)
+
+
+def read_kmeans(save_path="../model/kmeans.pth"):
+    return torch.load(save_path)
+
+
+if __name__ == "__main__":
+    # get_feature_kmeans()
+    centers = read_kmeans()
+    print(centers)
